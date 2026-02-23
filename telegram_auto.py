e28733a966639c3cc04a9efa5e89b7d9e1e86f8e
ujingang89-png <ujingang89@gmail.com>
2026-02-24 08:15:34 +0900

    Update telegram_auto.py

--- telegram_auto.py
+++ telegram_auto.py
@@ -16,19 +16,27 @@ MESSAGE_1 = """일보 부탁드립니다~
 CHAT_ID_2 = "-1002467111151"
 MESSAGE_2 = "1 2 3 4 5 6 7 8"
 
-def send_message(chat_id, text):
+THREAD_ID_2 = 30
+
+def send_message(chat_id, text, thread_id=None):
     url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
     data = {
         "chat_id": chat_id,
         "text": text
     }
+
+    if thread_id is not None:
+        data["message_thread_id"] = thread_id
+
     requests.post(url, data=data)
 
 def job():
     print("메시지 전송:", datetime.now())
 
+    # 일반 방
     send_message(CHAT_ID_1, MESSAGE_1)
-    send_message(CHAT_ID_2, MESSAGE_2)
+
+    send_message(CHAT_ID_2, MESSAGE_2, THREAD_ID_2)
 
 schedule.every().day.at("12:00").do(job)
 
@@ -37,7 +45,7 @@ def home():
     return "Bot is running!"
 
 if __name__ == "__main__":
-    job()  # 서버 시작하자마자 1번 전송 (테스트용)
+    job()  # 서버 시작 테스트용 (나중에 지워도 됨)
 
     while True:
         schedule.run_pending()

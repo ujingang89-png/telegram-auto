f038c8f03b6209c6cda50e03c5e593b392b3c161
ujingang89-png <ujingang89@gmail.com>
2026-04-05 23:33:04 +0900

    Update telegram_auto.py

--- telegram_auto.py
+++ telegram_auto.py
@@ -31,15 +31,16 @@ MESSAGE_4 = "파트별 금주 논의사항 양식 올려주세요~!"
 CHAT_ID_5 = "-1002244734007"
 MESSAGE_5 = "주간회의 PPT 마무리해주세요~!"
 
-CHAT_ID_6 = "-1003851451653"
-MESSAGE_6 = "부장님 이번주차 진성신 올려주시기 바랍니다~!"
+CHAT_ID_JS = "-1003851451653"
+
+MSG_MON_10 = "부장님 이번주 진성신 범위는 어디일까요?"
+MSG_TUE = "진성신은 목요일 주간회의 전까지입니다."
+MSG_WED = "진성신은 목요일까지입니다, 주간회의 전까지 입니다."
+MSG_THU = "당일에 주시는 사유보고는 받지 않겠습니다. 주간회의 전까지 모두 마무리 부탁드립니다."
 
 def send_message(chat_id, text, thread_id=None):
-    url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
-    data = {
-        "chat_id": chat_id,
-        "text": text
-    }
+    url = f"https://api.telegram.org/bot8703437303:AAEsfMv3-HjuRZfU7VRAxMvlYm-9ML4IOdc/sendMessage"
+    data = {"chat_id": chat_id, "text": text}
 
     if thread_id is not None:
         data["message_thread_id"] = thread_id
@@ -110,43 +111,70 @@ def job_wednesday_2300():
         last_sent_date_wed = now_date
         send_message(CHAT_ID_5, MESSAGE_5)
 
-def job_monday_0015():
-    global last_sent_date_mon
-
+last_sent_mon = None
+def job_monday_10():
+    global last_sent_mon
     kst = datetime.now(pytz.timezone("Asia/Seoul"))
     now_date = kst.strftime("%Y-%m-%d")
+    if kst.weekday() == 0 and kst.strftime("%H:%M") == "10:00" and last_sent_mon != now_date:
+        last_sent_mon = now_date
+        send_message(CHAT_ID_JS, MSG_MON_10)
 
-    if (
-        kst.weekday() == 0 and
-        kst.strftime("%H:%M") in ["00:15", "00:16", "00:17"] and
-        last_sent_date_wed != now_date
-    ):
-        last_sent_date_mon = now_date
-        send_message(CHAT_ID_6, MESSAGE_6)
+last_sent_tue = None
+def job_tuesday():
+    global last_sent_tue
+    kst = datetime.now(pytz.timezone("Asia/Seoul"))
+    now_date = kst.strftime("%Y-%m-%d")
+    if kst.weekday() == 1 and kst.strftime("%H:%M") in ["10:00", "20:00"] and last_sent_tue != now_date:
+        last_sent_tue = now_date
+        send_message(CHAT_ID_JS, MSG_TUE)
+
+last_sent_wed = None
+def job_wednesday():
+    global last_sent_wed
+    kst = datetime.now(pytz.timezone("Asia/Seoul"))
+    now_date = kst.strftime("%Y-%m-%d")
+    if kst.weekday() == 2 and kst.strftime("%H:%M") in ["10:00", "20:00"] and last_sent_wed != now_date:
+        last_sent_wed = now_date
+        send_message(CHAT_ID_JS, MSG_WED)
+
+last_sent_thu = None
+def job_thursday():
+    global last_sent_thu
+    kst = datetime.now(pytz.timezone("Asia/Seoul"))
+    now_date = kst.strftime("%Y-%m-%d")
+    if kst.weekday() == 3 and kst.strftime("%H:%M") in ["10:00", "15:00", "21:00"] and last_sent_thu != now_date:
+        last_sent_thu = now_date
+        send_message(CHAT_ID_JS, MSG_THU)
+
+
+
+
 
 schedule.every().minute.do(job_if_kst)
 schedule.every().minute.do(job_saturday_2130)
 schedule.every().minute.do(job_thursday_2300)
 schedule.every().minute.do(job_wednesday_2300)
-schedule.every().minute.do(job_monday_0015)
+
+schedule.every().minute.do(job_monday_10)
+schedule.every().minute.do(job_tuesday)
+schedule.every().minute.do(job_wednesday)
+schedule.every().minute.do(job_thursday)
+
 
 @app.route('/')
 def home():
     return "Bot is running!"
-
 
 def run_scheduler():
     while True:
         schedule.run_pending()
         time.sleep(1)
 
-
 if __name__ == "__main__":
     threading.Thread(target=run_scheduler, daemon=True).start()
-
     port = int(os.environ.get("PORT", 10000))
     app.run(host="0.0.0.0", port=port)
-
 
 
 
